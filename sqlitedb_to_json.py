a81ac5857751cc22b52d2b88eb250969a3e663e1
devendra-scs <52280849+devendra-scs@users.noreply.github.com>
2023-06-12 17:52:42 +0530

    Add files via upload

---
+++ sqlitedb_to_json.py
@@ -0,0 +1,30 @@
+import csv
+import sqlite3
+
+#Script to covert sqlite to csv
+OUTPUT_FILE_NAME="Consolidate-Report.csv"
+
+def writeCSVRow(csvWriter, row):
+    csvWriter.writerow(row)
+    return
+
+def covert_sqlite_to_csv(soup, csvWriter, row):
+    query= "SELECT  RD.name, BIB, EVD.EventYear,Distance, FinishTime, Pace, RankOverall, RankCategory, EVD.EventCity, EVD.EventName FROM EventData ED,RunnersDetails RD, EventDetails EVD  WHERE ED.RunnersID=RD.ID  AND EVD.ID=ED.EventID"
+    row.clear()
+    cursor = conn.execute(query);
+    for row in cursor:
+        writeCSVRow(csvWriter, row)
+        
+    return
+
+csvFile= open(OUTPUT_FILE_NAME,  'w', newline='') 
+count=0
+
+row=['Name', 'BIB Number','Year','Distance','Finished Time', 'PACE(min/km)', 'Rank Overall', "Category Rank", "City", "EventName"]
+csvWriter = csv.writer(csvFile)
+writeCSVRow(csvWriter, row)
+conn = sqlite3.connect('RunningData.db')
+covert_sqlite_to_csv(conn,csvWriter,row)
+conn.close()
+csvFile.close()
+print("Successfully Coverted");
