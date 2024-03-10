5ed07b11bf2eeb14e496f573e8e40f3e2fb42324
Singh <devendra.singh1@dell.com>
2024-03-10 17:44:32 +0530

    Restored deleted functions.

--- db/dbutil.py
+++ db/dbutil.py
@@ -7,9 +7,9 @@ __version__ = "1.0.1"
 __maintainer__ = "Devendra"
 __email__ = "devendra.scs@gmail.com"
 __status__ = "Production"
-# This is test python program for we scrapping, I have used this to fetch running data from TCS Open10k event 
+# This is test python program for we scrapping, I have used this to fetch running data from TCS Open10k event
 # This will create .csv and .excel files after fetching data
-# This program is only for demonstrations and author do not held any responsiblity of any license breach or malfuncitons. 
+# This program is only for demonstrations and author do not held any responsiblity of any license breach or malfuncitons.
 
 from bs4 import BeautifulSoup
 import sqlite3
@@ -34,39 +34,39 @@ class DatabaseUtil:
 
     def insert_event_details(self, event_name, event_city, event_date, event_year, base_url):
         EventID=self.get_event_ID(event_name, event_city, event_year)
-        
+
         print("EventID:",EventID)
         if(EventID>0):
             #print("Event Already Found")
             return EventID
-        
+
         sql="INSERT INTO EventDetails(EventName, EventCity, EventDate, EventURL, EventYear) VALUES('"+event_name.upper()+"','"+event_city+"','"+event_date+"','"+base_url+"','"+event_year+"')"
         self.conn.execute(sql);
         self.conn.commit()
         EventID=self.get_event_ID( event_name, event_city, event_year);
-        
+
         return EventID;
 
     def get_runners_ID(self, name, gender):
         runners_id=0
-        sql="SELECT ID from RunnersDetails WHERE  Name='"+name.upper()+"' COLLATE NOCASE AND Gender='"+gender+"'" 
+        sql="SELECT ID from RunnersDetails WHERE  Name='"+name.upper()+"' COLLATE NOCASE AND Gender='"+gender+"'"
         cursor = self.conn.execute(sql);
         for row in cursor:
            runners_id = row[0]
         return runners_id;
-        
+
     def insert_runners_details(self, name, gender):
         runners_id=self.get_runners_ID( name, gender);
-            
+
         if(runners_id>0):
             return runners_id
         sql="INSERT INTO RunnersDetails (Name, Gender) VALUES ( '"+name.upper()+"', '"+gender+"')";
-        #print(sql)    
+        #print(sql)
         self.conn.execute(sql);
         self.conn.commit()
-        runners_id=self.get_runners_ID(name, gender);    
+        runners_id=self.get_runners_ID(name, gender);
         return runners_id;
-        
+
     def get_record_id_from_event_data_table(self, event_id, BIB):
         record_id=0
         sql="SELECT ID from EventData where EventId='"+str(event_id)+"' and BIB='"+str(BIB)+"'";
@@ -74,12 +74,12 @@ class DatabaseUtil:
         for row in cursor:
            record_id = row[0]
         return record_id;
-        
-    
+
+
     def insert_row_in_db(self, event_id, runners_id, BIB, NetTime, GunTime, OverallRank, Category, CategoryRank, GenderRank, Distance, url ):
         #Check if runners data for this event already present in database
         record_id = self.get_record_id_from_event_data_table(event_id, BIB)
-        
+
         if record_id >0:
            #print("Record already Present");
            return 0;
@@ -89,13 +89,13 @@ class DatabaseUtil:
         self.conn.execute(sql);
         self.conn.commit()
         return 1;
-    
+
     #   update row in database
     def update_row_in_db(self, event_id, record_id, runners_id, BIB, NetTime, GunTime, OverallRank, Category, CategoryRank, GenderRank, Distance, url ):
         #update row in database
         sql = "UPDATE EventData SET FinishTime='"+str(NetTime)+"', GunTime='"+str(GunTime)+"', OverallRank='"+str(OverallRank)+"', GenderRank='"+str(GenderRank)+"', Distance='"+str(Distance)+"', Category='"+str(Category)+"', CategoryRank='"+str(CategoryRank)+"', ResultURL='"+str(url)+"' WHERE ID='"+str(record_id)+"'"
-        #print("SQL",sql)        
-        
+        #print("SQL",sql)
+
         self.conn.execute(sql);
         self.conn.commit()
         return 1;
@@ -103,9 +103,29 @@ class DatabaseUtil:
     def get_event_all_record_list(self, event_id):
         # get all id and bib in asscending order of Id and push in dictionary
         record_list={}
-        sql="SELECT ID, BIB from EventData where EventID='"+str(event_id)+"' and ResultURL is  NULL ORDER BY BIB ASC"    
+        sql="SELECT ID, BIB from EventData where EventID='"+str(event_id)+"' and ResultURL is  NULL ORDER BY BIB ASC"
         cursor = self.conn.execute(sql)
         for row in cursor:
             record_list[row[0]]=row[1]
-        
+
         return record_list;
+
+    def Insert_splits_data(self,  event_id, runners_id, BIB, Distance, Time):
+        split_id = self.Get_splits_data( event_id, runners_id, BIB, Distance)
+        if split_id >0:
+            #print("Split Data already present")
+            return True
+        sql="INSERT INTO SplitsDetails(EventID, RunnersID, BIB, Distance, Time) VALUES('"+str(event_id)+"','"+str(runners_id)+"','"+str(BIB)+"','"+str(Distance)+"','"+str(Time)+"')"
+        #print("Spilits Insert SQL:",sql)
+        self.conn.execute(sql)
+        self.conn.commit()
+        return True
+
+    def Get_splits_data(self, event_id, runners_id, BIB, Distance):
+        id=0
+        sql="SELECT ID from SplitsDetails where EventID='"+str(event_id)+"' AND RunnersID='"+str(runners_id)+"' AND BIB='"+str(BIB)+"' AND Distance='"+str(Distance)+"'"
+        #print("SQL:",sql)
+        cursor = self.conn.execute(sql);
+        for row in cursor:
+           id = row[0]
+        return id;
--- web-scrapping.py
+++ web-scrapping.py
@@ -19,13 +19,13 @@ logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %
 
 #<BEGIN Modify>
 #Change below paramenter
-EVENT_NAME="Tata Mumbai Marathon 2024"
-EVENT_CITY="Mumbai"
-EVENT_DATE="JANUARY 21, 2024"
+EVENT_NAME="Karnataka State Police Golden Jubliee Run 2024"
+EVENT_CITY="Bangalore"
+EVENT_DATE="MARCH 10, 2024"
 EVENT_YEAR="2024"
-START_BIB_NUMBER=50000
-END_BIB_NUMBER=50001
-BASE_URL ='https://www.sportstimingsolutions.in/share.php?event_id=78282&bib='
+START_BIB_NUMBER=15314
+END_BIB_NUMBER=15315
+BASE_URL ='https://www.sportstimingsolutions.in/share.php?event_id=79089&bib='
 #<END Modify>
 
 http = urllib3.PoolManager(cert_reqs='CERT_NONE')
@@ -37,28 +37,28 @@ logging.info("Event ID:"+str(event_id))
 
 def parseAndWriteResponse(event_id, soup, bibNumber, url):
     name=soup.find("h3",{"class":"txt-color img-padding"})
-    
+
     if name is  None:
         return False
     #row=['BIB', 'Name', 'Finished Time', 'Chip Pace (min/km)', 'Rank Overall', "Category Rank", "Category"]
     name = name.text.strip()
-    
+
     finishedText= soup.find("td", {"class":"text-center neww-table he"})
     if finishedText is None:
         return False
 
     finishedTime= finishedText.text
     timers= soup.find_all("td", {"class":"text-center neww-table"})
-    
+
     pace= timers[0].text
     rankOverall= timers[1].text
     genderRank =""
-    if len(timers) >2:    
+    if len(timers) >2:
         genderRank = timers[2].text
     categoryRank=""
     if len(timers) >3:
         categoryRank= timers[3].text
-    
+
     thlist= soup.find_all("th", {"class":"text-center"})
     category = thlist[len(thlist)-1].text if thlist else ""
     #print("Category:", category)
@@ -90,7 +90,7 @@ def parseAndWriteResponse(event_id, soup, bibNumber, url):
 
     splits={}
     pace=""
-    
+
     #Write details into database
     runners_id = dbutil.insert_runners_details(name, Gender);
     #print(event_id, runners_id, bibNumber, finishedTime, "".format(), rankOverall, category, categoryRank, genderRank )
@@ -100,7 +100,7 @@ def parseAndWriteResponse(event_id, soup, bibNumber, url):
     #Interval	Gun Time	Chip Time	Chip Pace (min/km)	Speed
     for row in table:
         td = row.find_all('td')
-        idx=0 
+        idx=0
         for columns in td:
         # print("Col:",columns)
             val = columns.text.strip()
@@ -125,21 +125,21 @@ def process_bib(bibNumber):
     resultURL = BASE_URL + str(bibNumber)
     logging.info(" Fetching details of BIB:"+resultURL)
     if bibNumber % 100 == 0:
-        logging.info(" Fetching details of BIB:"+ bibNumber)    
+        logging.info(" Fetching details of BIB:"+ bibNumber)
     try:
         result = http.request('GET', resultURL)
         html = result.data
         if result.status == 200:
-            soup = BeautifulSoup(html, "html5lib")            
-            parseAndWriteResponse(event_id, soup, bibNumber, resultURL)            
+            soup = BeautifulSoup(html, "html5lib")
+            parseAndWriteResponse(event_id, soup, bibNumber, resultURL)
     except Exception as e:
         logging.error("Error occurred during HTTP request:", str(e))
-    return True    
+    return True
 
 with ThreadPoolExecutor(max_workers=5) as executor:
     bibNumbers = range(START_BIB_NUMBER, END_BIB_NUMBER)
     executor.map(process_bib, bibNumbers)
     logging.info("Thread pool completed succesfully")
 
-    
+
 logging.info("completed succesfully")
